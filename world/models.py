fd36372ceb2b26fd58f5dd47fa130185ac6559e2
Matthew Magee <matthewmagee17@gmail.com>
2020-11-11 19:02:39 +0000

    First and final commit, not all aspects working

---
+++ world/models.py
@@ -0,0 +1,69 @@
+from django.contrib.gis.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+    last_location = models.PointField(
+        editable=False,
+        blank=True,
+        null=True,
+        default=None
+    )
+
+    def __str__(self):
+        return f"{self.user}"
+
+
+class LoginForm(models.Model):
+    username = models.CharField("Enter Username", max_length=50)
+    password = models.CharField("Enter Password", max_length=50)
+
+
+class WorldBorder(models.Model):
+    fips = models.CharField(max_length=2, null=True)
+    iso2 = models.CharField(max_length=2)
+    iso3 = models.CharField(max_length=3)
+    un = models.IntegerField()
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.BigIntegerField()
+    region = models.IntegerField()
+    subregion = models.IntegerField()
+    lon = models.FloatField()
+    lat = models.FloatField()
+    mpoly = models.MultiPolygonField(srid=4326)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
+
+
+# Auto-generated `LayerMapping` dictionary for WorldBorder model
+worldborder_mapping = {
+    'fips': 'FIPS',
+    'iso2': 'ISO2',
+    'iso3': 'ISO3',
+    'un': 'UN',
+    'name': 'NAME',
+    'area': 'AREA',
+    'pop2005': 'POP2005',
+    'region': 'REGION',
+    'subregion': 'SUBREGION',
+    'lon': 'LON',
+    'lat': 'LAT',
+    'geom': 'MULTIPOLYGON',
+}
+
+
+@receiver(post_save, sender=get_user_model())
+def manage_user_profile(sender, instance, created, **kwargs):
+    try:
+        my_profile = instance.profile
+        my_profile.save()
+    except Profile.DoesNotExist:
+        Profile.objects.create(user=instance)
